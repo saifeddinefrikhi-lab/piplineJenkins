3aa326669ab6f373660a4dcbdd8ad2a8c2a99645
saifeddinefrikhi-lab <saifeddine.frikhi@esprit.tn>
2025-11-18 23:09:34 +0100

    Initial commit - Projet Maven avec structure de base

---
+++ src/main/java/tn/esprit/piplinejenkins/PiplineJenkinsApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.piplinejenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiplineJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiplineJenkinsApplication.class, args);
+    }
+
+}
